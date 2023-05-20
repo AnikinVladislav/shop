633948ce2fec2ca3517da6cc793a8ff5d3635ce7
ToughMelomaniac <114314923+ToughMelomaniac@users.noreply.github.com>
2023-05-20 18:17:08 +0300

    added model product, superuser admin, requirements.txt

--- therapeuticMud/shop/models.py
+++ therapeuticMud/shop/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+class Product(models.Model):
+    """
+    price - decimal_places=2 - количество знаков после запятой
+    """
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+    quantify = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
