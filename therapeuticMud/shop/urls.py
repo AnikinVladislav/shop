c3d4542d0717e341c173971663017a74d3474eb3
AnikinVladislav <vlad9anikin1999@gmail.com>
2023-05-21 18:04:45 +0300

    finish with card

--- therapeuticMud/shop/urls.py
+++ therapeuticMud/shop/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('/<int:id>', views.show, name='show')
+    path('<int:id>', views.show, name='show'),
+    path('add_to_cart/<int:product_id>', views.add_to_cart, name='add_to_cart'),
+    path('show_cart', views.show_cart, name='show_cart'),
+    path('remove_from_cart/<int:product_in_cart_id>', views.remove_from_cart, name='remove_from_cart'),
+    path('add_to_cart_plus/<int:product_id>', views.add_to_cart_plus, name='add_to_cart_plus'),
+    path('remove_to_cart_minus/<int:product_id>', views.remove_to_cart_minus, name='remove_to_cart_minus'),
 ]
--- therapeuticMud/shop/views.py
+++ therapeuticMud/shop/views.py
@@ -1,38 +1,93 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models import Q
 from .models import *
 
-def index(requset):
+def index(request):
     prod = Product.objects.all().values()
     context = {
         'products': prod
     }
-    return render(requset, 'shop/index.html', context)
+    return render(request, 'shop/index.html', context)
 
 
-def show(requset, id):
-    print(id)
+def show(request, id):
     prod = Product.objects.get(id = id)
-    print(prod)
     context = {
         'myproduct': prod
     }
-    return render(requset, 'shop/decription_prod.html', context)
+    return render(request, 'shop/decription_prod.html', context)
 
 
-def add_to_cart(request, product_id, quantify):
+def add_to_cart(request, product_id):
     if request.user.is_authenticated:
-        product_in_cart = ProductInCart(user_id=request.user.id, product_id=product_id, quantify=quantify)
-        product_in_cart.save()
-
-        # Redirect to a success page or display a message
-        return redirect('success_page')
+        myuser = User.objects.get(id=request.user.id)
+        myproduct =Product.objects.get(id=product_id)
+        product_in_cart_obj = ProductInCart.objects.filter(Q(user_id=request.user.id) & Q(product_id=product_id)).values()
+        if product_in_cart_obj:
+            temp = product_in_cart_obj[0]['quantify'] + 1
+            ProductInCart.objects.filter(Q(user_id=request.user.id) & Q(product_id=product_id)).update(quantify = temp)
+        else:
+            product_in_cart = ProductInCart(user_id=myuser, product_id=myproduct, quantify=1)
+            product_in_cart.save()
+        messages.success(request, (f"Товар: {myproduct.name} добавлен в корзину"))
+        return redirect('home')
     else:
-        return redirect('login_page')
+        return redirect('login')
+    
+
+def add_to_cart_plus(request, product_id):
+    if request.user.is_authenticated:
+        myuser = User.objects.get(id=request.user.id)
+        myproduct =Product.objects.get(id=product_id)
+        product_in_cart_obj = ProductInCart.objects.filter(Q(user_id=request.user.id) & Q(product_id=product_id)).values()
+        if product_in_cart_obj:
+            temp = product_in_cart_obj[0]['quantify'] + 1
+            ProductInCart.objects.filter(Q(user_id=request.user.id) & Q(product_id=product_id)).update(quantify = temp)
+        else:
+            product_in_cart = ProductInCart(user_id=myuser, product_id=myproduct, quantify=1)
+            product_in_cart.save()
+        return redirect('show_cart')
+    else:
+        return redirect('login')
+    
+
+def remove_to_cart_minus(request, product_id):
+    if request.user.is_authenticated:
+        myuser = User.objects.get(id=request.user.id)
+        myproduct =Product.objects.get(id=product_id)
+        product_in_cart_obj = ProductInCart.objects.filter(Q(user_id=request.user.id) & Q(product_id=product_id)).values()
+        if product_in_cart_obj:
+            temp = product_in_cart_obj[0]['quantify'] - 1
+            ProductInCart.objects.filter(Q(user_id=request.user.id) & Q(product_id=product_id)).update(quantify = temp)
+        else:
+            product_in_cart = ProductInCart(user_id=myuser, product_id=myproduct, quantify=1)
+            product_in_cart.save()
+        return redirect('show_cart')
+    else:
+        return redirect('login')
+    
+
+def show_cart(request):
+    total = 0
+    if request.user.is_authenticated:
+        myuser = User.objects.get(id=request.user.id)
+        product_in_cart_obj = ProductInCart.objects.filter(user_id=request.user.id).values('id','product_id','product_id__name', 'product_id__price', 'quantify')
+        for prod in product_in_cart_obj:
+            prod['calculated_value'] = prod['quantify'] * prod['product_id__price']
+            total += (prod['quantify'] * prod['product_id__price'])
+        context = {
+            'products': product_in_cart_obj,
+            'total': total
+        }
+        return render(request, 'shop/cart.html', context)
+    else:
+        return redirect('login')
 
 
 def remove_from_cart(request, product_in_cart_id):
+    print(product_in_cart_id)
     product_in_cart = ProductInCart.objects.get(id=product_in_cart_id)
     product_in_cart.delete()
-
-    #show_cart()
-    # return render(request, 'cart.html', )
+    return redirect('show_cart')
